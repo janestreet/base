905c612d42c638edae50631ba90a37230dea3a54
Xavier Clerc <xclerc@janestreet.com>
2019-12-06 13:21:44 +0000

    Tentative patch for OCaml 4.10 support.

--- src/exn_stubs.c
+++ src/exn_stubs.c
@@ -1,4 +1,14 @@
 #include <caml/mlvalues.h>
+#include <caml/version.h>
+
+#if OCAML_VERSION >= 41000
+
+CAMLprim value Base_clear_caml_backtrace_pos () {
+  Caml_state_field(backtrace_pos) = 0;
+  return Val_unit;
+}
+
+#else
 
 extern int caml_backtrace_pos;
 
@@ -6,3 +16,5 @@ CAMLprim value Base_clear_caml_backtrace_pos () {
   caml_backtrace_pos = 0;
   return Val_unit;
 }
+
+#endif
