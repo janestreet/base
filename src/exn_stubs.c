86cdbd3face99eb98bf2adbbee4c3469c3bf41db
Xavier Clerc <xclerc@janestreet.com>
2020-02-24 12:54:44 +0000

    v0.14-preview.122.09+286

--- src/exn_stubs.c
+++ src/exn_stubs.c
@@ -1,11 +1,6 @@
+#define CAML_INTERNALS
 #include <caml/mlvalues.h>
-#include <caml/version.h>
-
-#if OCAML_VERSION < 41000
-
-extern int caml_backtrace_pos;
-
-#endif
+#include <caml/backtrace.h>
 
 CAMLprim value Base_clear_caml_backtrace_pos () {
   caml_backtrace_pos = 0;
