7d1761f40f8becc0db9d8e949464d57697188e78
Aaron L. Zeng <me@bcc32.com>
2022-02-08 12:07:43 -0500

    Reduce number of copies in String.copy and apply ocamlformat
    Signed-off-by: Aaron L. Zeng <me@bcc32.com>

--- src/string0.ml
+++ src/string0.ml
@@ -16,6 +16,7 @@
    ocamldep from mistakenly causing a file to depend on [Base.String]. *)
 
 open! Import0
+module Bytes = Bytes0
 module Sys = Sys0
 
 module String = struct
@@ -32,7 +33,13 @@ let max_length = Sys.max_string_length
 let ( ^ ) = ( ^ )
 let capitalize = Caml.String.capitalize_ascii
 let compare = Caml.String.compare
-let copy x = Caml.Bytes.to_string (Caml.Bytes.copy (Caml.Bytes.of_string x))
+
+let copy x =
+  Bytes.unsafe_to_string
+    ~no_mutation_while_string_reachable:
+      (Bytes.copy (Bytes.unsafe_of_string_promise_no_mutation x))
+;;
+
 let escaped = Caml.String.escaped
 let lowercase = Caml.String.lowercase_ascii
 let make = Caml.String.make
