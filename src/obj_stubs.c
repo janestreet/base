83493d8ac800dcfe53b804afe9b1062ef16160a1
Dmitry Bely <dmitry.bely@gmail.com>
2026-05-07 10:50:25 +0100

    Support MSVC compiler on Windows

--- src/obj_stubs.c
+++ src/obj_stubs.c
@@ -1,5 +1,11 @@
 #include <caml/alloc.h>
 #include <caml/mlvalues.h>
+
+#ifdef _MSC_VER
+#define __attribute__(attr)
+#pragma warning(disable: 4100) // Unreferenced formal parameter
+#endif
+
 // only used for public release;
 // internally this is implemented by caml_dummy_obj_is_stack in compiler runtime
 CAMLprim value caml_dummy_obj_is_stack(__attribute__((unused)) value blk) {
